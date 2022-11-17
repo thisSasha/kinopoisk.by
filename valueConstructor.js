e51f571f15322740057575be8d34c87ee1d19c07
Саня <108116977+thisSasha@users.noreply.github.com>
2022-11-17 19:29:58 +0300

    Add files via upload

--- valueConstructor.js
+++ valueConstructor.js
@@ -2,10 +2,31 @@ export { ValueObject }
 function ValueObject(params) {
     this.value = params.value;
     this.object = params.object;
-    this.defaultText = params.defaultText;
-    this.object.innerHTML = this.defaultText;
+    function checkDefaultText(thi) {
+        let x = params.defaultText.split('&');
+        x.forEach(el => {
+            if (el == 'value') {
+                x[x.indexOf(el)] = thi.value;
+                x = x.join(' ');
+            };
+        });
+
+        thi.defaultText = x;
+        thi.object.innerHTML = thi.defaultText;
+    };
+    checkDefaultText(this);
+    this.substractValue = function (value) {
+        this.value -= value;
+        this.object.innerHTML = this.defaultText;
+        checkDefaultText(this);
+    };
     this.setValue = function (value) {
         this.value = value;
         this.object.innerHTML = this.defaultText;
+        checkDefaultText(this);
+    };
+    this.addValue = function (value) {
+        this.value += value;
+        checkDefaultText(this);
     };
 };
